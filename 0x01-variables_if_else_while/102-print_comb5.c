78cbb5a33395b644fdcf3e838585e760752f806a
Mo2Hefny <moamensaied1@gmail.com>
2022-09-09 12:36:35 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return:0 print combination
+ */
+
+int main(void)
+{
+	int i, j, n, m;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (n = i; n < 58; n++)
+			{
+				for (m = j + 1; m < 58; m++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(n);
+					putchar(m);
+					if (i == 57 && j == 56)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
