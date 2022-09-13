93c646656da2248bbac1066c40ebb42cfc30b784
Mo2Hefny <moamensaied1@gmail.com>
2022-09-13 13:05:59 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - print numbers
@@ -6,28 +6,13 @@
  */
 void print_to_98(int n)
 {
-	int digit;
-
 	while (n != 98)
 	{
-		digit = _abs(n);
-		if (n < 0)
-		{
-			_putchar('-');
-		}
-		if (digit > 99)
-			_putchar('0' + (digit /= 100));
-		if (digit > 9)
-			_putchar('0' + (digit /= 10));
-		_putchar('0' + digit);
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 		if (n > 98)
 			n--;
 		else
 			n++;
 	}
-	_putchar('0' + 9);
-	_putchar('0' + 8);
-	_putchar('\n');
+	printf("%d\n", n);
 }
