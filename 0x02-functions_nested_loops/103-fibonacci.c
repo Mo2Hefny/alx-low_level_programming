3728e20caba308b0ba7058090218d76d0a898000
Mo2Hefny <moamensaied1@gmail.com>
2022-09-13 13:52:51 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long n1 = 1;
+	unsigned long n2 = 2;
+	unsigned long n3 = 3
+	unsigned long long sum = 2;
+	int i;
+
+	for (i = 0; n3 <= 4000000; i++)
+	{
+		n3 = n2 + n1;
+		if (n3 % 2 == 0)
+			sum += n3;
+		n1 = n2;
+		n2 = n3;
+	}
+	printf("%llu\n", sum);
+	return (0);
+}
