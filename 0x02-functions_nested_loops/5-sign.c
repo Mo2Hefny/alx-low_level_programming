c8f08caa374cef0472a45ba059669184316c56c5
Mo2Hefny <moamensaied1@gmail.com>
2022-09-13 09:48:12 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - checks number
+ * @n: number input
+ * Return:1 if positive, 0 if zero, and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
