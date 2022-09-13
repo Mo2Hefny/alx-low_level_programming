f887bfd5ebd6e99454dde60ea7e731ceb6af40fc
Mo2Hefny <moamensaied1@gmail.com>
2022-09-13 12:34:37 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdlih>
+#include "main.h"
+/**
+ * print_to_98 - print numbers
+ * @n: input number
+ */
+void print_to_98(int n)
+{
+	int digit;
+
+	while (n != 98)
+	{
+		digit = _abs(n);
+		if (n < 0)
+		{
+			_putchar('-');
+		}
+		if (digit > 99)
+			_putchar('0' + (digit /= 100));
+		if (digit > 9)
+			_putchar('0' + (digit /= 10));
+		_putchar('0' + digit);
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	_putchar('0' + 9);
+	_putchar('0' + 8);
+	_putchar('\n');
+}
