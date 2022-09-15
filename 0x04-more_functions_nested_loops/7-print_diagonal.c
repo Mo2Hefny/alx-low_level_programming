10f670a19bd0c223eb43dcb529ee5e78ced80fb1
Mo2Hefny <moamensaied1@gmail.com>
2022-09-15 07:12:52 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,6 +10,8 @@ void print_diagonal(int n)
 int i, j;
 if (n <= 0)
 _putchar('\n');
+else
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j <= i; j++)
@@ -18,5 +20,6 @@ _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
+}
 }
 }
