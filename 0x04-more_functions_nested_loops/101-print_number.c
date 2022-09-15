c967ffdbf8471a148331a8d50c04c8841bf1124f
Mo2Hefny <moamensaied1@gmail.com>
2022-09-15 12:31:29 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,26 +8,13 @@
 void print_number(int n)
 {
 int num = n;
-int temp = n;
-int div = 1;
 
 if (n < 0)
 {
 _putchar('-');
-temp = -n;
+num = -n;
 }
-else if (n == 0)
-_putchar('0' + 0);
-while (num != 0)
-{
-div *= 10;
-num /= 10;
-}
-while (div / 10 != 0)
-{
-div /= 10;
-num = temp / div;
-_putchar('0' + num);
-temp -= num *div;
-}
+if (num / 10 > 0)
+print_number(num / 10);
+_putchar('0' + num % 10);
 }
