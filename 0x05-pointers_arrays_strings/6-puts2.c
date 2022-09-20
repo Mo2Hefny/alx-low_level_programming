02ff52572dda536d351d3bb70bacec31e91601b8
Mo2Hefny <moamensaied1@gmail.com>
2022-09-19 18:15:45 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - skips every other char
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+_putchar(str[i]);
+_putchar('\n');
+}
