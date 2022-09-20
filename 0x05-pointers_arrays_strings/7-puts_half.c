882a8e0d4df8c424dedd0824fb99322fe4b66cb3
Mo2Hefny <moamensaied1@gmail.com>
2022-09-19 18:41:04 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the second half of string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+int MAX = strlen(str);
+int i;
+
+for (i = MAX / 2; i < MAX; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
