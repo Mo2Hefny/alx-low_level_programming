e6c9911f53f45877ea34236b1b1040af74ab2346
Mo2Hefny <moamensaied1@gmail.com>
2022-09-19 18:35:04 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,9 +8,13 @@
 
 void rev_string(char *s)
 {
-char *temp;
-int i = strlen(s) - 1;
-while (i > -1)
-temp += s[i];
-s = temp;
+char temp;
+int i;
+int MAX = strlen(s) - 1;
+for (i = 0; i <= MAX / 2; i++)
+{
+temp = s[i];
+s[i] = s[MAX - i];
+s[MAX - i] = temp;
+}
 }
