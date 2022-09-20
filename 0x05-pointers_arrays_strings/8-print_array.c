dadde591ff49722d26589603af1c1e6ac60b85da
Mo2Hefny <moamensaied1@gmail.com>
2022-09-19 18:47:34 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - print number of elements
+ * @a: array
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n)
+{
+while (n > 0)
+{
+_putchar(a);
+if (n != 1)
+{
+_putchar(',');
+_putchar(' ');
+}
+a++;
+n--;
+}
+_putchar('\n');
+}
