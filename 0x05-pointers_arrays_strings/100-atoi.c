bfa8992cfa2414b2b43d788886bd7b2fe5824ed0
Mo2Hefny <moamensaied1@gmail.com>
2022-09-19 19:33:27 -0700

    11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,17 +10,15 @@ int _atoi(char *s)
 {
 int count = 0;
 int num = 0;
-unsigned int n = 1;
 unsigned int i;
-for (i = 0; s[i] < 48 || s[i] > 57; i++)
+for (i = 0; (s[i] < 48 || s[i] > 57) && s[i] != '\0'; i++)
 {
 if (s[i] == '-')
 count++;
 }
 for ( ; s[i] >= 48 && s[i] <= 57; i++)
 {
-num *= n;
-n *= 10;
+num *= 10;
 num += (s[i] - '0');
 }
 if (count % 2 != 0)
