2126a0a1b60ba6fad7301b74c40e94ea333803f4
Mo2Hefny <moamensaied1@gmail.com>
2022-09-19 18:11:00 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+char *temp;
+int i = strlen(s) - 1;
+while (i > -1)
+temp += s[i];
+s = temp;
+}
