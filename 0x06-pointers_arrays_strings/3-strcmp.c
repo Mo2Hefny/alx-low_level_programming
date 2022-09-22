666daea95c44c6ea6f44e729642ad7d4532d356f
Mo2Hefny <moamensaied1@gmail.com>
2022-09-22 09:51:42 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compare characters of strings
  * @s1: string 1
  * @s2: string 2
- * Return: 15 if s1 > s2, 0 if s1 = s2, -15 if s1 < s2
+ * Return: diff
  */
 
 int _strcmp(char *s1, char *s2)
@@ -12,9 +12,11 @@ int _strcmp(char *s1, char *s2)
 int i, diff = 0;
 for (i = 0; s1[i] || s2[i]; i++)
 {
+if (s1[i] != s2[i])
+{
 diff = s1[i] - s2[i];
-if (!diff)
 return (diff);
+}
 }
 return (diff);
 }
