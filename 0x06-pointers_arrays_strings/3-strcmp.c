94ac1276e02f4eb97462af4d1e71daab663c0aca
Mo2Hefny <moamensaied1@gmail.com>
2022-09-22 09:49:11 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,11 @@
 int _strcmp(char *s1, char *s2)
 {
 int i, diff = 0;
-for (i = 0; s1[i] && s2[i]; i++)
+for (i = 0; s1[i] || s2[i]; i++)
 {
 diff = s1[i] - s2[i];
 if (!diff)
 return (diff);
 }
-diff = s1[i] - s2[i];
 return (diff);
 }
