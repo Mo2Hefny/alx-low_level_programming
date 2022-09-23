7e341337ca0ae1ec560e1b484e544bcc9e248f5a
Mo2Hefny <moamensaied1@gmail.com>
2022-09-22 19:39:58 -0700

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,33 +11,20 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = 0, len2 = 0;
-int last_dig = 0;
-int i;
-int added = 0;
-while (n1[len1] || n2[len2])
-{
-if (n1[len1])
+int i, added = 0, len1 = 0, len2 = 0, last_dig = 0;
+while (n1[len1])
 len1++;
-if (n2[len2])
+while (n2[len2])
 len2++;
-}
 for (i = 1; len1 - i >= 0 || len2 - i >= 0; i++)
 {
 if (len1 - i >= 0)
-{
 added += n1[len1 - i] - '0';
-}
 if (len2 - i >= 0)
-{
 added += n2[len2 - i] - '0';
-}
 r[size_r - i] = (added % 10) + '0';
 last_dig++;
-if (added >= 10)
-added = 1;
-else
-added = 0;
+added /= 10;
 }
 if (added)
 {
@@ -45,20 +32,14 @@ last_dig++;
 r[0] = '1';
 }
 if (size_r - last_dig <= 0)
-{
 return (0);
-}
 added = 0;
 for (i = 1; i <= last_dig; i++)
 {
 if (len1 - i >= 0)
-{
 added += n1[len1 - i] - '0';
-}
 if (len2 - i >= 0)
-{
 added += n2[len2 - i] - '0';
-}
 r[last_dig - i] = (added % 10) + '0';
 if (added >= 10)
 added = 1;
