a5c1ae3d4adc61760c838b3a0917d971ad6d98c2
Mo2Hefny <moamensaied1@gmail.com>
2022-09-26 14:56:38 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: string
+ * @accept: characters to check
+ * Return: position
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, len = 0;
+int found;
+while (s[len])
+len++;
+for (i = 0; i < len; i++)
+{
+found = 0;
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+break;
+}
+}
+if (!found)
+return (i);
+}
+return (i - 1);
+}
