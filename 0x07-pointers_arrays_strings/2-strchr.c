eec5c1aedf36ad570d3b489942c931bd0347af21
Mo2Hefny <moamensaied1@gmail.com>
2022-09-26 14:27:51 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a character in a string.
+ * @s: string
+ * @c: character to locate
+ * Return: string
+ */
+
+char *_strchr(char *s, char c)
+{
+int i, len = 0;
+while (s[len])
+len++;
+for (i = 0; i < len; i++)
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (0);
+}
