b12de409cf5b58980294ce5ff0213ebe9bf5958f
Mo2Hefny <moamensaied1@gmail.com>
2022-09-30 05:30:21 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of string of argumments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
