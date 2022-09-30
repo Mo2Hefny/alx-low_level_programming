4473e03ed3a5a847fa9e0a2e18249a2b94a4b163
Mo2Hefny <moamensaied1@gmail.com>
2022-09-30 06:22:56 -0700

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * min - calculate min number of cents needed
+ * @n: change
+ * Return: min number of cents.
+ */
+int min(int n)
+{
+if (n >= 25)
+return (1 + min(n - 25));
+if (n >= 10)
+return (1 + min(n - 10));
+if (n >= 5)
+return (1 + min(n - 5));
+if (n >= 2)
+return (1 + min(n - 2));
+if (n >= 1)
+return (1 + min(n - 1));
+return (0);
+}
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of string of argumments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int change;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+change = atoi(argv[1]);
+if (change < 0)
+{
+printf("0\n");
+return (1);
+}
+printf("%d\n", min(change));
+return (0);
+}
