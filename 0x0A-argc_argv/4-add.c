1be467cf0d8fde67414dd5ee3638787fa2dfe921
Mo2Hefny <moamensaied1@gmail.com>
2022-09-30 05:57:23 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * isnumber - check if string is a number
+ * @s: string
+ * Return: 1 if true, 0 otherwise
+ */
+int isnumber(char *s)
+{
+if(!*s)
+return (1);
+if (*s >= '0' && *s <= '9')
+return (isnumber(++s));
+return (0);
+}
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of string of argumments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int res = 0, i;
+for (i = 1; i < argc; i++)
+{
+if (!isnumber(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+res += atoi(argv[i]);
+}
+printf("%d\n", res);
+return (0);
+}
