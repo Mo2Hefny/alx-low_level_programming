2e421ac6398cf8e8a1ef7cf049ad8df3d2f034c0
Mo2Hefny <moamensaied1@gmail.com>
2022-10-06 12:37:08 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,17 +12,14 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
-unsigned int i, len1 = 0, len2 = 0, size;
+unsigned int i, len1 = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 while (s1[len1])
 len1++;
-while (s2[len2])
-len2++;
-size = len1 + len2;
-str = malloc(size + 1);
+str = malloc(len1 + n + 1);
 if (str == NULL)
 return (NULL);
 for (i = 0; s1[i]; i++)
