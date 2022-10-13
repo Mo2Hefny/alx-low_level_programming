4b5a4b3a1452d61dbe879f9e2335a635141099f9
Mo2Hefny <moamensaied1@gmail.com>
2022-10-12 19:48:53 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: string of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, res;
+int (*p)(int, int);
+char *op;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+op = argv[2];
+if ((*op == '/' || *op == '%') && !b)
+{
+printf("Error\n");
+exit(100);
+}
+if (get_op_func(op) == NULL)
+{
+printf("Error");
+exit(99);
+}
+p = get_op_func(op);
+res = p(a, b);
+printf("%d\n", res);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: summition
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - substracts two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - modulus two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
