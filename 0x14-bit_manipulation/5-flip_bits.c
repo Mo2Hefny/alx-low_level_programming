28c3b4aaa62da6db75cab73c8f7562c1baafd7a3
Mo2Hefny <moamensaied1@gmail.com>
2022-10-27 13:35:50 -0700

    5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int i, count = 0;
+int i, count = 0;
 unsigned long int current;
 unsigned long int change = n ^ m;
 
