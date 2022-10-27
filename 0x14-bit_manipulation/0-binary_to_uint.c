3c3e5635814459d82262c82bb431a468e212cafa
Mo2Hefny <moamensaied1@gmail.com>
2022-10-27 13:55:12 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int i, num = 0, w = 1;
-
-if ((*b != '1' && *b != '0') || !b)
+int i, w = 1;
+unsigned int num = 0;
+if (!b)
 return (0);
-for (i = 1; b[i]; i++)
+for (i = 0; b[i]; i++)
 w *= 2;
 for (i = 0; b[i]; i++)
 {
-num += b[i] * w;
+if (b[i] != '1' && b[i] != '0')
+return (0);
 w /= 2;
+num += (b[i] - '0') * w;
 }
 return (num);
 }
