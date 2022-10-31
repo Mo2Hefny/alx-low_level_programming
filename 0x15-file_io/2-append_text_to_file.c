e74d80ef0014631a59dd92c383327037f807caff
Mo2Hefny <moamensaied1@gmail.com>
2022-10-31 16:55:31 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,12 +15,13 @@ int o, w, len = 0;
 
 if (!filename)
 return (-1);
-while (text_content[len++])
-;
+if (text_content != NULL)
+while (text_content[len])
+len++;
 o = open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len - 1);
+w = write(o, text_content, len);
 if (o == -1 || w == -1)
 return (-1);
-close (o);
+close(o);
 return (1);
 }
