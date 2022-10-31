13c7bbd36d1ffe30e329caf2d16f66a9c3279c17
Mo2Hefny <moamensaied1@gmail.com>
2022-10-31 16:44:48 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: Text to add to file.
+ * Return: If the function fails - -1.
+ *         On success - 1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int o, w, len = 0;
+
+if (!filename)
+return (-1);
+while (text_content[len++])
+;
+o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600)
+w = write(o, text_content, len - 1);
+if (o == -1 || w == -1)
+return (-1);
+close (o);
+return (1);
+}
