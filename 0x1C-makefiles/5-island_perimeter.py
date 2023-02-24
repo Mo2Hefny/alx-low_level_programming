25d13d5550527860e0a68651cc63a19a6af17e20
Mo2Hefny <moamensaied1@gmail.com>
2023-02-24 09:49:12 -0800

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
-                if i == 0 or not g[i - 1][j]:
+                if i == 0 or not grid[i - 1][j]:
                     perimeter += 1
-                if i == len(grid) - 1 or not g[i + 1][j]:
+                if i == len(grid) - 1 or not grid[i + 1][j]:
                     perimeter += 1
-                if j == 0 or not g[i][j - 1]:
+                if j == 0 or not grid[i][j - 1]:
                     perimeter += 1
-                if j == len(grid[i]) - 1 or not g[i][j + 1]:
+                if j == len(grid[i]) - 1 or not grid[i][j + 1]:
                     perimeter += 1
     return perimeter
